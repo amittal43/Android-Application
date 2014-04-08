d70de310d47e5a0c9ae00c39c15175264769866a
ksucipto1609@gmail.com <ksucipto1609@gmail.com@be060e4c-8ef1-e090-6078-7e5dce4176fb>
2014-04-08 01:52:00 +0000

    Add Borrow and Lend buttons in MainActivity

--- comp/src/com/example/comp/LendOption.java
+++ comp/src/com/example/comp/LendOption.java
@@ -3,65 +3,32 @@ package com.example.comp;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-import android.widget.Toast;
 import android.os.Build;
 
-public class ShowProduct extends Activity {
+public class LendOption extends Activity {
 
-
-    int category;
-    int index;
-    
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_show_product);
+		setContentView(R.layout.activity_lend_option);
 
 		/*if (savedInstanceState == null) {
 			getFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}*/
-		Bundle bundle = getIntent().getExtras();
-	    //Extract each value from the bundle for usage
-	    category = bundle.getInt("CATEGORY");
-	    index = bundle.getInt("INDEX");
-		
-	    TextView textTitle = (TextView) findViewById(R.id.showproductTitle);
-	    textTitle.append(Listing.bookListing.get(index).getProduct().getTitle());
-	    
-	    TextView textDescription = (TextView) findViewById(R.id.showproductDescription);
-	    textDescription.append(Listing.bookListing.get(index).getProduct().getDescription());
-	    
-	    TextView textQuality = (TextView) findViewById(R.id.showproductQuality);
-	    textQuality.append(Listing.bookListing.get(index).getProduct().getQuality());
-	    
-	    TextView textPrice = (TextView) findViewById(R.id.showproductPrice);
-	    String price = Double.toString(Listing.bookListing.get(index).getPrice());
-	    textPrice.append(price);
-		
-	}
-	
-	public void buy (View view){
-		Intent intent = new Intent(this, MainActivity.class);
-		startActivity(intent);
-		Toast.makeText(this, "You have successfully bought the item!",Toast.LENGTH_LONG).show();
-		Listing.bookListing.remove(index);
-		
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.show_product, menu);
+		getMenuInflater().inflate(R.menu.lend_option, menu);
 		return true;
 	}
 
@@ -88,7 +55,7 @@ public class ShowProduct extends Activity {
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
 				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_show_product,
+			View rootView = inflater.inflate(R.layout.fragment_lend_option,
 					container, false);
 			return rootView;
 		}
