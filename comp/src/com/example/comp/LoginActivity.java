5aa341821c8a11bc401c32f05261e7c43c2bfbd9
mittaladitya13 <mittaladitya13@be060e4c-8ef1-e090-6078-7e5dce4176fb>
2014-04-08 05:21:49 +0000

    added the login

---
+++ comp/src/com/example/comp/LoginActivity.java
@@ -0,0 +1,186 @@
+package com.example.comp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.login, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_login,
+					container, false);
+			return rootView;
+		}
+	}
+	
+	public void onClick(View view) 
+	{
+		switch(view.getId()){
+		case R.id.login:
+			// call AsynTask to perform network operation on separate thread
+			if(validateFormInput())
+			{
+				EditText username = (EditText) findViewById(R.id.username2);
+				EditText password = (EditText) findViewById(R.id.password2);
+				new HttpAsyncTask().execute("http://ihome.ust.hk/~sraghuraman/cgi-bin/login.php", username.getText().toString(), password.getText().toString());
+			}
+			else
+				Toast.makeText(this, "Incomplete Data", Toast.LENGTH_SHORT).show();
+			break;
+		}
+	}
+	
+	private boolean validateFormInput()
+	{
+		EditText username = (EditText) findViewById(R.id.username2);
+		EditText password = (EditText) findViewById(R.id.password2);
+		if(username.getText().toString().length() <= 0 || 
+				password.getText().toString().length() <= 0)
+		{
+			return false;
+		}
+		
+		return true;
+	}
+
+	private class HttpAsyncTask extends AsyncTask<String, Void, String> {
+		@Override
+		protected String doInBackground(String...urls) {
+			return POST(urls[0], urls[1], urls[2]);
+		}
+		// onPostExecute displays the results of the AsyncTask.
+		
+		@Override
+		protected void onPostExecute(String result) 
+		{
+			Toast.makeText(getBaseContext(), "Data Sent!" + " " + result , Toast.LENGTH_LONG).show();
+		}
+	}
+
+
+	public static String POST(String url, String user, String password){
+		InputStream inputStream = null;
+		String result = "";
+		try {
+
+			// 1. create HttpClient
+			HttpClient httpclient = new DefaultHttpClient();
+
+			// 2. make POST request to the given URL
+			HttpPost httpPost = new HttpPost(url);
+
+			
+			// ** Alternative way to convert Person object to JSON string usin Jackson Lib 
+			// ObjectMapper mapper = new ObjectMapper();
+			// json = mapper.writeValueAsString(person); 
+
+			// 5. set json to StringEntity
+
+			//StringEntity se = new StringEntity(json);
+
+			// Request parameters and other properties.
+			List<NameValuePair> params = new ArrayList<NameValuePair>(2);
+			params.add(new BasicNameValuePair("username", user));
+			params.add(new BasicNameValuePair("password", password));
+			httpPost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
+
+			// 8. Execute POST request to the given URL
+			HttpResponse httpResponse = httpclient.execute(httpPost);
+
+			// 9. receive response as inputStream
+			inputStream = httpResponse.getEntity().getContent();
+
+			// 10. convert inputstream to string
+			if(inputStream != null)
+				result = convertInputStreamToString(inputStream);
+			else
+				result = "Did not work!";
+
+		} catch (Exception e) {
+			Log.d("InputStream", e.getLocalizedMessage());
+		}
+
+		// 11. return result
+		return result;
+	}
+	
+	/*private void printJSONObject(JSONObject js)
+	{
+		System.out.println(js.toString());
+	}*/
+
+	private static String convertInputStreamToString(InputStream inputStream) throws IOException{
+		BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+		String line = "";
+		String result = "";
+		while((line = bufferedReader.readLine()) != null)
+			result += line;
+		inputStream.close();
+		return result;
+
+	}   
+
+
+
+}
