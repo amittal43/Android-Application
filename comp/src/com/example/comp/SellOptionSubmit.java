ae0e35eebd3b8745aa3714f22467fc461c91aab6
czarsath93 <czarsath93@be060e4c-8ef1-e090-6078-7e5dce4176fb>
2014-04-11 15:20:15 +0000

    Using database for storing products.

--- comp/src/com/example/comp/SellOptionSubmit.java
+++ comp/src/com/example/comp/SellOptionSubmit.java
@@ -1,9 +1,26 @@
 package com.example.comp;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
 import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,7 +31,7 @@ import android.widget.Toast;
 
 public class SellOptionSubmit extends Activity {
 
-    
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -22,34 +39,34 @@ public class SellOptionSubmit extends Activity {
 
 		if (savedInstanceState == null) {
 			getFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
+			.add(R.id.container, new PlaceholderFragment()).commit();
 		}
-		
-	    Bundle bundle = getIntent().getExtras();
-	    //Extract each value from the bundle for usage
-	    String title = bundle.getString("TITLE");
-	    String category = bundle.getString("CATEGORY");
-	    String quality = bundle.getString("QUALITY");
-	    String description = bundle.getString("DESCRIPTION");
-	    String price = Double.toString(bundle.getDouble("PRICE"));
-	    
-	    
-	    TextView textTitle = (TextView) findViewById(R.id.confirmTitle);
-	    textTitle.append(title);
-	    
-	    TextView textCategory = (TextView) findViewById(R.id.confirmCategory);
-	    textCategory.append(category);
-	    
-	    TextView textQuality = (TextView) findViewById(R.id.confirmQuality);
-	    textQuality.append(quality);
-	    
-	    TextView textDescription = (TextView) findViewById(R.id.confirmDescription);
-	    textDescription.append(description);
-	    
-	    TextView textPrice = (TextView) findViewById(R.id.confirmPrice);
-	    textPrice.append(price);
+
+		Bundle bundle = getIntent().getExtras();
+		//Extract each value from the bundle for usage
+		String title = bundle.getString("TITLE");
+		String category = bundle.getString("CATEGORY");
+		String quality = bundle.getString("QUALITY");
+		String description = bundle.getString("DESCRIPTION");
+		String price = Double.toString(bundle.getDouble("PRICE"));
+
+
+		TextView textTitle = (TextView) findViewById(R.id.confirmTitle);
+		textTitle.append(title);
+
+		TextView textCategory = (TextView) findViewById(R.id.confirmCategory);
+		textCategory.append(category);
+
+		TextView textQuality = (TextView) findViewById(R.id.confirmQuality);
+		textQuality.append(quality);
+
+		TextView textDescription = (TextView) findViewById(R.id.confirmDescription);
+		textDescription.append(description);
+
+		TextView textPrice = (TextView) findViewById(R.id.confirmPrice);
+		textPrice.append(price);
 	}
-	
+
 	/**
 	 * Called when the user clicks the confirm button
 	 * Go back to MainActivity (Menu) page
@@ -57,10 +74,20 @@ public class SellOptionSubmit extends Activity {
 	 */
 	public void backToHome(View view){
 		Intent intent = new Intent(this, MainActivity.class);
-		startActivity(intent);
 		Toast.makeText(this, "The Item is added successfully",Toast.LENGTH_LONG).show();
+		Bundle bundle = getIntent().getExtras();
+		//Extract each value from the bundle for usage
+		String title = bundle.getString("TITLE");
+		String category = bundle.getString("CATEGORY");
+		String quality = bundle.getString("QUALITY");
+		String description = bundle.getString("DESCRIPTION");
+		String price = String.valueOf(bundle.getDouble("PRICE"));
+		new HttpAsyncTask().execute("http://ihome.ust.hk/~sraghuraman/cgi-bin/add-item-to-database.php", 
+				title, category,price, quality, description);
+		
+		startActivity(intent);
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
@@ -97,5 +124,73 @@ public class SellOptionSubmit extends Activity {
 			return rootView;
 		}
 	}
+
+	class HttpAsyncTask extends AsyncTask<String, Void, String> {
+		@Override
+		protected String doInBackground(String...urls) {
+			return POST(urls[0], urls[1], urls[2], urls[3], urls[4], urls[5]);
+		}
+		// onPostExecute displays the results of the AsyncTask.
+		@Override
+		protected void onPostExecute(String result) {
+			Toast.makeText(getBaseContext(), "Data Sent!" + " " +  result, Toast.LENGTH_LONG).show();
+		}
+	}
+
+
+	public static String POST(String url, String title, String category, String price, String quality, String desc){
+		InputStream inputStream = null;
+		String result = "";
+		try {
+
+			// 1. create HttpClient
+			HttpClient httpclient = new DefaultHttpClient();
+
+			// 2. make POST request to the given URL
+			HttpPost httpPost = new HttpPost(url);
+
+			String json = "";
+
+			// Request parameters and other properties.
+			List<NameValuePair> params = new ArrayList<NameValuePair>(2);
+			params.add(new BasicNameValuePair("title", title));
+			params.add(new BasicNameValuePair("category", category));
+			params.add(new BasicNameValuePair("price", price.toString()));
+			params.add(new BasicNameValuePair("quality", quality.toString()));
+			params.add(new BasicNameValuePair("description", desc.toString()));
+			
+			httpPost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
+
+			// 8. Execute POST request to the given URL
+			HttpResponse httpResponse = httpclient.execute(httpPost);
+
+			// 9. receive response as inputStream
+			inputStream = httpResponse.getEntity().getContent();
+
+			// 10. convert inputstream to string
+			if(inputStream != null)
+				result = convertInputStreamToString(inputStream);
+			else
+				result = "Did not work!";
+
+		} catch (Exception e) {
+			Log.d("InputStream", e.getLocalizedMessage());
+		}
+
+		// 11. return result
+		return result;
+	}
+
+	private static String convertInputStreamToString(InputStream inputStream) throws IOException{
+		BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+		String line = "";
+		String result = "";
+		while((line = bufferedReader.readLine()) != null)
+			result += line;
+		inputStream.close();
+		return result;
+
+	}   
+
 
 }
